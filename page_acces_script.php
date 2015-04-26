01e2c67a6ac404311a9aebb43e5e3142b7d5321c
defaultnick01 <matusmatusovsky@gmail.com>
2015-04-26 20:31:21 +0200

    Pridaný ping príkaz
    Dokončiť program

--- page_acces_script.php
+++ page_acces_script.php
@@ -14,4 +14,17 @@ $nahodne_cislo = rand(0,$pocet_stranok-1);
 // do premennej $homepage nacitaj obsah stranky
 $homepage = file_get_contents( $stranky[$nahodne_cislo] );
 echo $homepage;
+// ping funkcia
+function pingAddress($ip){
+    $pingresult = shell_exec("start /b ping $ip -n 1");
+    $dead = "Request timed out.";
+    $deadoralive = strpos($dead, $pingresult);
+
+    if ($deadoralive == false){
+        echo "The IP address, $ip, is dead";
+    } else {
+        echo "The IP address, $ip, is alive";
+    }
+
+}
 ?>
